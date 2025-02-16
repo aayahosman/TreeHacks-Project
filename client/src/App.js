ff0853e5a322eec05ff6f0adf27540319c7a58b9
vichedanarith <vichedanarith2027@u.northwestern.edu>
2025-02-15 19:29:41 -0800

    zoom api server is compatible with client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import SwipeCard from "./components/SwipeCard";
 import SwipeMatch from "./components/SwipeMatch";
 import Home from "./components/Home";
 import Login from "./components/Login.jsx";
 import Profile from "./pages/SignUp";
 import LookingFor from './pages/LookingFor.jsx';
+import Matched from './components/Matched.jsx';
 import profilesData from "./json/profiles.json";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Messages from "./pages/Messages";
@@ -12,22 +12,18 @@ import Messages from "./pages/Messages";
 const App = () => {
   const [profilesQueue, setProfilesQueue] = useState(profilesData);
 
-  const handleSwipe = (id, direction) => {
-    console.log(`${profilesQueue[0].name} swiped ${direction}`);
-    setProfilesQueue((prevQueue) => prevQueue.slice(1));
-  };
-
   return (
     <BrowserRouter>
     <div className="container">
         <Routes>
-          <Route index element={<Home />} />
+          <Route index element={<Matched />} />
           <Route path="/home" element={<Home />}></Route>
           <Route path="/profile" element={<Profile />} />
           <Route path="/messages" element={<Messages />} />
           <Route path="/login" element={<Login />} />
           <Route path="/lookingfor" element={<LookingFor />} />
           <Route path="/swipe-match" element={<SwipeMatch />} />
+          {/* <Route path="/zoom-meeting" element={<ZoomMeeting />} /> */}
         </Routes>
     </div>
     </BrowserRouter>
